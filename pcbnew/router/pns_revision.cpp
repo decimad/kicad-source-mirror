cba0b756ff6f5619b205c2fddf9cba9bb3e96707
decimad <michsteinb@gmail.com>
2016-08-31 00:45:01 +0200

    Add class PNS::REVISION that will serve to factor revision tree management out of PNS::NODE

---
+++ pcbnew/router/pns_revision.cpp
@@ -0,0 +1,293 @@
+/*
+* REVISION - helper class for the KiRouter
+*
+* Copyright (C) 2016 KiCad Developers, see AUTHORS.txt for contributors.
+* Author: Michael Steinberg <michsteinb@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cassert>
+#include <algorithm>
+#include "pns_item.h"
+#include "pns_revision.h"
+
+namespace PNS {
+
+    //
+    // CHANGE_SET
+    //
+    void CHANGE_SET::Clear()
+    {
+        m_added_items.clear();
+        m_removed_items.clear();
+    }
+
+    void CHANGE_SET::Apply( const REVISION* aState )
+    {
+        for( auto& item : aState->AddedItems() )
+        {
+            Add( item.get() );
+        }
+
+        for( auto item : aState->RemovedItems() )
+        {
+            Remove( item );
+        }
+    }
+
+    void CHANGE_SET::Revert( const REVISION* aState )
+    {
+        for( auto& item : aState->AddedItems() )
+        {
+            Remove( item.get() );
+        }
+
+        for( auto item : aState->RemovedItems() )
+        {
+            Add( item );
+        }
+    }
+
+    void CHANGE_SET::Add( const ITEM* aItem )
+    {
+        auto it = std::find( m_removed_items.begin(), m_removed_items.end(), aItem );
+        if( it != m_removed_items.end() )
+        {
+            m_removed_items.erase( it );
+        }
+        else
+        {
+            m_added_items.push_back( aItem );
+        }
+    }
+
+    void CHANGE_SET::Remove( const ITEM* aItem )
+    {
+        auto it = std::find( m_added_items.begin(), m_added_items.end(), aItem );
+
+        if( it != m_added_items.end() )
+        {
+            m_added_items.erase( it );
+        }
+        else
+        {
+            m_removed_items.push_back( aItem );
+        }
+    }
+
+    CHANGE_SET CHANGE_SET::FromPath( std::vector< const REVISION* > aPath )
+    {
+        CHANGE_SET result;
+        std::reverse( aPath.begin(), aPath.end() );
+
+        for( auto diffstate : aPath ) {
+            result.Apply( diffstate );
+        }
+
+        return result;
+    }
+
+    SEQUENCE< CHANGE_SET::ITEM_ITERATOR > CHANGE_SET::AddedItems()
+    {
+        return{ m_added_items.begin(), m_added_items.end() };
+    }
+
+    SEQUENCE< CHANGE_SET::CONST_ITEM_ITERATOR > CHANGE_SET::AddedItems() const
+    {
+        return{ m_added_items.begin(), m_added_items.end() };
+    }
+
+    SEQUENCE< CHANGE_SET::ITEM_ITERATOR > CHANGE_SET::RemovedItems()
+    {
+        return{ m_removed_items.begin(), m_removed_items.end() };
+    }
+
+    SEQUENCE< CHANGE_SET::CONST_ITEM_ITERATOR > CHANGE_SET::RemovedItems() const
+    {
+        return{ m_removed_items.begin(), m_removed_items.end() };
+    }
+
+
+    //
+    // DiffState
+    //
+
+    REVISION::REVISION()
+        : m_parent( nullptr )
+    {
+    }
+
+    REVISION::~REVISION()
+    {
+    }
+
+    void REVISION::AddItem( std::unique_ptr< ITEM > aItem )
+    {
+        m_added_items.push_back( std::move( aItem ) );
+    }
+
+    void REVISION::RemoveItem( ITEM* aItem )
+    {
+        auto it = std::find_if(
+            m_added_items.begin(),
+            m_added_items.end(),
+            [aItem]( const std::unique_ptr< ITEM >& ptr ) { return ptr.get() == aItem; }
+        );
+
+        if( it != m_added_items.end() )
+        {
+            m_added_items.erase( it );
+        }
+        else
+        {
+            // maybe check if the item is here already
+            m_removed_items.push_back( aItem );
+        }
+    }
+
+    bool REVISION::IsShadowed( const ITEM* aItem )
+    {
+        bool here = std::find( m_removed_items.begin(), m_removed_items.end(), aItem )
+            != m_removed_items.end();
+        return here || (m_parent && m_parent->IsShadowed( aItem ));
+    }
+
+    std::unique_ptr< REVISION > REVISION::ReleaseBranch( const REVISION* aBranch )
+    {
+        auto it = std::find_if(
+            m_branches.begin(),
+            m_branches.end(),
+            [aBranch]( const std::unique_ptr< REVISION >& ref ) { return ref.get() == aBranch; }
+        );
+
+        if( it != m_branches.end() )
+        {
+            auto ptr = std::move( *it );
+            m_branches.erase( it );
+            ptr->m_parent = nullptr;
+            return ptr;
+        }
+        else
+        {
+            return{};
+        }
+    }
+
+    void REVISION::Absorb( REVISION* aDiff )
+    {
+        for( auto* item_ptr : aDiff->m_removed_items )
+        {
+            RemoveItem( item_ptr );
+        }
+        aDiff->m_removed_items.clear();
+
+        for( auto& item_ptr : aDiff->m_added_items )
+        {
+            AddItem( std::move( item_ptr ) );
+        }
+        aDiff->m_added_items.clear();
+    }
+
+    REVISION * REVISION::Squash()
+    {
+        assert( m_parent );
+        m_parent->Absorb( this );
+
+        // Releasing this branch will set parent_ to nullptr, so copy it early.
+        auto parent = m_parent;
+        auto myself = m_parent->ReleaseBranch( this );
+
+        parent->ClearBranches();
+
+        parent->m_branches.insert( parent->m_branches.end(),
+            std::make_move_iterator( m_branches.begin() ),
+            std::make_move_iterator( m_branches.end() )
+            );
+
+        return parent;
+    }
+
+    void REVISION::ClearBranches()
+    {
+        m_branches.clear();
+    }
+
+    REVISION* REVISION::Branch()
+    {
+        m_branches.emplace_back( std::unique_ptr< REVISION >( new REVISION() ) );
+        m_branches.back()->m_parent = this;
+        return m_branches.back().get();
+    }
+
+    void REVISION::RemoveBranch( REVISION* aBranch )
+    {
+        ReleaseBranch( aBranch );
+    }
+
+    const REVISION* REVISION::Parent() const
+    {
+        return m_parent;
+    }
+
+    size_t REVISION::NumChanges() const
+    {
+        return m_added_items.size() + m_removed_items.size();
+    }
+
+    std::vector<const REVISION*> REVISION::Path( const REVISION * aAncestor ) const
+    {
+        std::vector< const REVISION* > result;
+        const REVISION* state = this;
+
+        while( state != aAncestor )
+        {
+            result.push_back( state );
+            state = state->Parent();
+        }
+
+        return result;
+    }
+
+    SEQUENCE< REVISION::ADDED_ITEM_ITERATOR > REVISION::AddedItems()
+    {
+        return{ m_added_items.begin(), m_added_items.end() };
+    }
+
+    SEQUENCE< REVISION::CONST_ADDED_ITEM_ITERATOR > REVISION::AddedItems() const
+    {
+        return{ m_added_items.begin(), m_added_items.end() };
+    }
+
+    SEQUENCE< REVISION::REMOVED_ITEM_ITERATOR> REVISION::RemovedItems()
+    {
+        return{ m_removed_items.begin(), m_removed_items.end() };
+    }
+
+    SEQUENCE< REVISION::CONST_REMOVED_ITEM_ITERATOR> REVISION::RemovedItems() const
+    {
+        return{ m_removed_items.begin(), m_removed_items.end() };
+    }
+
+    SEQUENCE< REVISION::BRANCH_ITERATOR > REVISION::Branches()
+    {
+        return{ m_branches.begin(), m_branches.end() };
+    }
+
+    SEQUENCE< REVISION::CONST_BRANCH_ITERATOR > REVISION::Branches() const
+    {
+        return{ m_branches.begin(), m_branches.end() };
+    }
+
+}
---
+++ pcbnew/router/pns_revision.h
@@ -0,0 +1,247 @@
+/*
+* REVISION - helper class for the KiRouter
+*
+* Copyright (C) 2016 KiCad Developers, see AUTHORS.txt for contributors.
+* Author: Michael Steinberg <michsteinb@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PNS_DIFF_STATE_H__
+#define PNS_DIFF_STATE_H__
+
+#include <memory>
+#include <vector>
+
+namespace PNS {
+
+    /// REVISION Overview
+    /// REVISIONs track the add-/remove-ITEM actions in form of a revision-tree and
+    /// manage the lifetime of the added ITEMs.
+    /// One can branch every revision, drop branches, squash changes into parent revisions etc.
+    /// The class helps implementing revisions of states with a dynamic set of immutable items,
+    /// as is the case with the spatial state of PNS::NODE, that is used throughout to implement
+    /// the push&shove routing.
+    /// -> Could possibly be made a template.
+
+    class ITEM;
+    class REVISION;
+
+    ///
+    /// Class SEQUENCE
+    /// Wraps up a [start, end) iterator sequence.
+    ///
+
+    template< typename ITERATOR >
+    class SEQUENCE
+    {
+    public:
+        SEQUENCE( ITERATOR aBegin, ITERATOR aEnd )
+            : m_begin( std::move( aBegin ) ), m_end( std::move( aEnd ) )
+        {}
+
+        SEQUENCE( SEQUENCE&& ) = default;
+        SEQUENCE& operator=( SEQUENCE&& ) = default;
+
+        size_t size() const {
+            return std::distance( m_begin, m_end );
+        }
+
+        const ITERATOR& begin() {
+            return m_begin;
+        }
+
+        const ITERATOR& end() {
+            return m_end;
+        }
+
+    private:
+        ITERATOR m_begin, m_end;
+    };
+
+    ///
+    /// Class CHANGE_SET
+    /// Holds aggregated changes over multiple revisions, non-owned items
+    /// Can be generated out of a revision-path.
+    ///
+
+    class CHANGE_SET {
+    private:
+        using ITEMS_CONTAINER   = std::vector< const ITEM* >;
+
+    public:
+        using ITEM_ITERATOR       = ITEMS_CONTAINER::iterator;
+        using CONST_ITEM_ITERATOR = ITEMS_CONTAINER::const_iterator;
+
+    public:
+        void Clear();
+        void Apply ( const REVISION* aState );
+        void Revert( const REVISION* aState );
+
+        void Add   ( const ITEM* aItem );
+        void Remove( const ITEM* aItem );
+
+        static CHANGE_SET FromPath( std::vector< const REVISION* > aPath );
+
+        SEQUENCE< ITEM_ITERATOR >       AddedItems();
+        SEQUENCE< CONST_ITEM_ITERATOR > AddedItems() const;
+
+        SEQUENCE< ITEM_ITERATOR >       RemovedItems();
+        SEQUENCE< CONST_ITEM_ITERATOR > RemovedItems() const;
+
+    private:
+        ITEMS_CONTAINER m_added_items;
+        ITEMS_CONTAINER m_removed_items;
+    };
+
+    ///
+    /// Class REVISION
+    /// Tracks differences of world revisions and manages all ITEM's lifetimes
+    ///
+
+    class REVISION {
+    public:
+        REVISION();
+        ~REVISION();
+
+    private:
+        using ADDED_ITEMS_CONTAINER   = std::vector< std::unique_ptr< ITEM > >;
+        using REMOVED_ITEMS_CONTAINER = std::vector< ITEM* >;
+        using BRANCHES_CONTAINER      = std::vector< std::unique_ptr< REVISION > >;
+
+    public:
+        using ADDED_ITEM_ITERATOR         = ADDED_ITEMS_CONTAINER::iterator;
+        using CONST_ADDED_ITEM_ITERATOR   = ADDED_ITEMS_CONTAINER::const_iterator;
+
+        using REMOVED_ITEM_ITERATOR       = REMOVED_ITEMS_CONTAINER::iterator;
+        using CONST_REMOVED_ITEM_ITERATOR = REMOVED_ITEMS_CONTAINER::const_iterator;
+
+        using BRANCH_ITERATOR             = BRANCHES_CONTAINER::iterator;
+        using CONST_BRANCH_ITERATOR       = BRANCHES_CONTAINER::const_iterator;
+
+    public:
+        /**
+         * Function AddItem
+         * Add an item to this revision. The revision tree takes ownership.
+         * @note Undefined behaviour if this revision is not a leaf.
+         * @param aItem to be added to the revision tree
+         */
+        void AddItem( std::unique_ptr< ITEM > aItem );
+
+        /**
+         * Function RemoveItem
+         * Remove aItem if it was added in this revision or shadow it otherwise
+         * @note Undefined behaviour if this revision is not a leaf.
+         * @param aItem to be be removed or shadowed in this revision's sub-tree
+         */
+        void RemoveItem( ITEM* aItem );
+
+        /**
+         * Function IsShadowed
+         * Checks if aItem is alive but shadowed (removed in a revision including this one)
+         * @return true iff the item is shadowed
+         */
+        bool IsShadowed( const ITEM* aItem );
+
+        /**
+        * Function Squash
+        * Squash this revision into parent.
+        * @note Undefined behaviour if this revision is a root revision
+        * @return REVISION* - parent of this revision.
+        * @pre  Parent() != nullptr
+        * @post this revision is deleted
+        */
+        REVISION* Squash();
+
+        /**
+        * Function ClearBranches
+        * Remove all branches of this revision, thereby deleting all items introduced
+        * below this revision.
+        * @post Branches().size() == 0
+        */
+        void ClearBranches();
+
+        /**
+         * Function Branch
+         * Create a new branch of this revision and return a pointer to it.
+         * @return REVISION* pointer to the created branch
+         * @post retval->Parent() == this
+         */
+        REVISION* Branch();
+
+        /**
+         * Function RemoveBranch
+         * Remove a branch from this revision.
+         * @note Undefined behaviour if aBranch is not a branch of this revision.
+         * @param aBranch Pointer to a branch
+         */
+        void RemoveBranch( REVISION* aBranch );
+
+        /**
+         * Function ReleaseBranch
+         * Release a branch from this revision and return an owning pointer to it.
+         * @note Undefined behaviour if aBranch is not a branch of this revision.
+         * @param aBranch Pointer to a branch
+         * @return Owning pointer to the former branch.
+         */
+        std::unique_ptr< REVISION > ReleaseBranch( const REVISION* aBranch );
+
+        /**
+         * Function Parent
+         * Returns the parent of this branch or nullptr if this revision is a root
+         * @return Const pointer to parent revision or nullptr if root
+         */
+        const REVISION* Parent() const;
+
+        /**
+         * Function NumChanges
+         * Return the aggregate number of individual non-cancelling changes in this revision
+         * @return Aggregate number of individual non-cancelling changes in this revision
+         */
+        size_t NumChanges() const;
+
+        /**
+         * Function Path
+         * Returns a path of DIFF_STATEs from this revision to an ancestor revion. Paths are
+         * always directed towards the root of the revision tree.
+         * @note Undefined behaviour if aAncestor is not an ancestor of this revision
+         * @pre Parent()[->Parent()*] == aAncestor
+         */
+        std::vector< const REVISION* > Path( const REVISION* aAncestor ) const;
+
+
+        //
+        // Sequence accessors for { added items; removed items; branches }
+        //
+        SEQUENCE< ADDED_ITEM_ITERATOR >       AddedItems();
+        SEQUENCE< CONST_ADDED_ITEM_ITERATOR > AddedItems() const;
+
+        SEQUENCE< REMOVED_ITEM_ITERATOR >       RemovedItems();
+        SEQUENCE< CONST_REMOVED_ITEM_ITERATOR > RemovedItems() const;
+
+        SEQUENCE< BRANCH_ITERATOR >       Branches();
+        SEQUENCE< CONST_BRANCH_ITERATOR > Branches() const;
+    private:
+        void Absorb( REVISION* aDiff );
+
+        REVISION* m_parent;
+        std::vector< std::unique_ptr< REVISION > > m_branches;
+
+        std::vector< std::unique_ptr< ITEM > > m_added_items;
+        std::vector< ITEM* >                   m_removed_items;
+    };
+
+}
+
+#endif
